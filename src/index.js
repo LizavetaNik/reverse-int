caf049031e1bb5d66ad051ea0089ddf5616d6258
VIS-developmentIT <vis-developmentIT@vis.ua>
2023-01-05 13:19:26 +0200

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    
+    if( n < 0 )
+    {
+        n = n *(-1);
+    }
+
+    const std = (n).toString();
+    let newStd = '';
+    
+    for( let i = Number( (n).toString().length - 1); i >= 0; i--){
+        newStd = newStd + std[i];
+    }
+     
+    return Number( newStd );
 }
